a0ed47efe24bba557b02fb25b95f962ba0b8db2f
Tim Vaughan <tgvaughan@gmail.com>
2015-12-06 17:43:15 +1300

    Added unit tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,22 @@
-using TimeTree
+using TimeTrees
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+# Test newick parsing:
+t = TimeTree("((A:1,B:1):1,C:2):0;")
+r = t.root
+
+# Test getNodes and getLeaves
+nodes = getNodes(t)
+leaves = getLeaves(t)
+l = leaves[1]
+
+# Test basic methods
+@test isRoot(r) == true
+@test isRoot(l) == false
+@test isLeaf(r) == false
+@test isLeaf(l) == true
+
+# Test tree sorting
+@test getDecendentCount(r) == 5
+s = getSorted(t)
+@test s.root.children[1].label == "C"
